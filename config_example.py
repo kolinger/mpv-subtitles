84fa8adf163a3d2b3849dc60fe5fd7ffd2ea5bc7
Tomáš Kolinger <tomas@kolinger.name>
2024-10-12 16:11:37 +0200

    updated subliminal to make it work again

--- config_example.py
+++ config_example.py
@@ -1,16 +1,28 @@
+import logging
 import os
 
 from babelfish import Language
 
-cache_path = os.path.join(os.path.dirname(__file__), "cache")
+project_dir = os.path.realpath(os.path.dirname(__file__))
+cache_path = os.path.join(project_dir, "cache")
 cache_lifetime = 3600 * 12
+log_level = logging.INFO
 
 # languages needs to be ISO 639-3 https://cs.wikipedia.org/wiki/Seznam_k%C3%B3d%C5%AF_ISO_639-1
 # eng, ces, ...
 languages = {Language("eng")}
-providers = ["podnapisi", "opensubtitles", "addic7ed"]
+providers = [
+    "podnapisi",
+    "opensubtitles",
+    "opensubtitlescom",
+    "addic7ed",
+    "gestdown",
+    "tvsubtitles",
+    "napiprojekt",
+]
 providers_auth = {
     "opensubtitles": {"username": "user", "password": "mlask"},
+    "opensubtitlescom": {"username": "email", "password": "mlask"},
     "addic7ed": {"username": "user", "password": "mlask"},
 }
 avoid_hearing_impaired = True
--- subtitles.py
+++ subtitles.py
@@ -8,10 +8,13 @@ from sys import argv
 from time import time
 from typing import Any
 
-from subliminal import scan_video, list_subtitles, region, compute_score, download_subtitles
+from subliminal import scan_video, region, compute_score, download_subtitles, list_subtitles
 from subliminal.providers.addic7ed import Addic7edSubtitle
+from subliminal.providers.napiprojekt import NapiProjektSubtitle
 from subliminal.providers.opensubtitles import OpenSubtitlesSubtitle
+from subliminal.providers.opensubtitlescom import OpenSubtitlesComSubtitle
 from subliminal.providers.podnapisi import PodnapisiSubtitle
+from subliminal.providers.tvsubtitles import TVsubtitlesSubtitle
 
 import config
 
@@ -46,8 +49,9 @@ def run():
                 {video}, config.languages,
                 providers=config.providers, provider_configs=config.providers_auth
             )
-            with open(cache_file, "wb") as file:
-                pickle.dump((video, subtitles), file)
+            if len(subtitles[video]) > 0:
+                with open(cache_file, "wb") as file:
+                    pickle.dump((video, subtitles), file)
 
         items = []
         zeros = []
@@ -60,17 +64,32 @@ def run():
                 episode = subtitle.episode
                 year = subtitle.year
             elif isinstance(subtitle, OpenSubtitlesSubtitle):
-                name = subtitle.movie_name
+                name = subtitle.movie_name if subtitle.movie_name else subtitle.series_name
+                season = subtitle.series_season
+                episode = subtitle.series_episode
+                year = subtitle.movie_year
+            elif isinstance(subtitle, OpenSubtitlesComSubtitle):
+                name = subtitle.movie_title
                 season = subtitle.series_season
                 episode = subtitle.series_episode
                 year = subtitle.movie_year
             elif isinstance(subtitle, Addic7edSubtitle):
-                name = subtitle.title
+                name = subtitle.series
                 season = subtitle.season
                 episode = subtitle.episode
                 year = subtitle.year
+            elif isinstance(subtitle, TVsubtitlesSubtitle):
+                name = subtitle.series
+                season = subtitle.season
+                episode = subtitle.episode
+                year = subtitle.year
+            elif isinstance(subtitle, NapiProjektSubtitle):
+                name = "direct match - %s (napiproject)" % str(subtitle.subtitle_id)
+                season = None
+                episode = None
+                year = None
             else:
-                continue
+                raise Exception("unknown subtitle: %s" % subtitle)
 
             if season and episode:
                 name += " S%sE%s" % (str(season).zfill(2), str(episode).zfill(2))
@@ -134,11 +153,25 @@ def run():
 
 
 try:
+    if "ENABLE_LOGGING" in os.environ:
+        logger = logging.getLogger()
+        logger.setLevel(config.log_level)
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        console = logging.StreamHandler(sys.stdout)
+        console.setLevel(config.log_level)
+        console.setFormatter(formatter)
+        logger.addHandler(console)
+
+        if config.log_level <= logging.DEBUG:
+            for name in logging.root.manager.loggerDict:
+                logger = logging.getLogger(name)
+                logger.setLevel(logging.DEBUG)
+                logger.addHandler(console)
+
     run()
+
 except KeyboardInterrupt:
     exit(1)
-except SystemExit:
-    raise
-except:
-    logging.exception(sys.exc_info()[0])
+except Exception as e:
+    logging.exception(e)
     exit(1)
